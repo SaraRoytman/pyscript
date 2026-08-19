cbc13c2b6ab786542f0c95865be292c60b0df7d6
sara2 <sara270498@gmail.com>
2026-08-19 14:03:06 +0300

    commit

--- pyscript.py
+++ pyscript.py
@@ -1,6 +1,6 @@
 import os
-
-folder = input("Enter folder path: ")
+print("checking")
+folder = input("Enter your folder path: ")
 
 for i, filename in enumerate(os.listdir(folder), start=1):
     old_path = os.path.join(folder, filename)
@@ -8,3 +8,4 @@ for i, filename in enumerate(os.listdir(folder), start=1):
     new_path = os.path.join(folder, new_name)
     os.rename(old_path, new_path)
     print(f"{filename} -> {new_name}")
+    
